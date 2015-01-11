1c9e589d9fb4e774d6d090846f531933ab637296
jcarter48 <jacobcarter@jacobs-mbp.fullsail.com>
2015-01-10 22:08:13 -0500

    Day 3 Lab Activity
    I huh really messed up somewhere, raged and deleted and started over
    but now everything is working so thats great.

--- Day3LabExpressions/Day3LabExpressions.js
+++
@@ -1,6 +0,0 @@
-/*
- Jacob Carter
- 1/10/2015
- Day 3 Lab Expressions
- */
-
---
+++ Day3LabExpressions/js/script.js
@@ -0,0 +1,14 @@
+/*
+ Jacob Carter
+ 1/10/2015
+ Day 3 Lab Expressions
+ */
+
+//Calculate how many pieces of pizza each partygoer gets.
+var slices = 10;
+var people = 50;
+var pizzas = 10;
+
+//Multiply the amount of pizzas by the amount of slices then divide that by the number of people
+var totalSlices = pizzas * slices;
+console.log(totalSlices);

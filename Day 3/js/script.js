80c743cc01704e99220850f2152d9782951ffe0b
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-10 21:14:11 -0500

    Concatenation and Constants
    Concatenation and Constants added into our javascript file.

--- Day 3/js/script.js
+++ Day 3/js/script.js
@@ -142,5 +142,33 @@ var average = (quiz1 + quiz2 + quiz3) /3;
 console.log(average);
 
 //Do NOT overuse parenthesis
-// var perimeter = length *2 + width *2;
+// var perimeter = ((length*2) + (width*2));
+// var perimeter = length*2 + width*2;
 
+//Concatenation - combining text strings
+// use the + plus sign for text and not just numbers
+
+var firstName = "Kermit";
+var lastName = "The Frog";
+
+//Concatenate the first and last name into a full name
+var fullName = firstName +" "+ lastName;
+console.log(fullName);
+
+console.log(fullName + " is my favorite muppet!");
+
+var d = "6";
+var e = "7";
+
+var combine = d + e;
+console.log(combine);
+
+//Constant values are always UPPERCASE
+var PI = 3.14;
+
+//Round to a decimal place
+// toFixed(#) - # is how many decimal places we want
+
+var num = 5.6734325567;
+num = num.toFixed(2);
+console.log(num);

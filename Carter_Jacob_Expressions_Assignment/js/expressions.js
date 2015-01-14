5935e2795a8e415876b9b9e2a4c0917aa809dcd7
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-13 22:53:59 -0500

    Misunderstood the assignment
    Committing this because I spent so much time doing something that won’t
    work for the assignment……

--- Carter_Jacob_Expressions_Assignment/js/expressions.js
+++ Carter_Jacob_Expressions_Assignment/js/expressions.js
@@ -6,8 +6,29 @@
  */
 //alert("Testing to see if it works");
 
+//Create array for the roommates
+var roomMates = ["Alexa, Levi, Jacob"];
 
+// Test the array
+console.log(roomMates);
 
+//Calculate total monthly apartment cost for the three roommates Alexa, Levi, and Jacob.
+total = rent + electric + water
+var rent = prompt ("Let's calculate how much money it cost each person to live in an apartment this month. \n Please enter in how much rent is:");
+var electric = prompt("Please enter how much the electric bill is:");
+var water = prompt("Please enter how much the water bill is:");
+
+//Calculate total
+var totalApartment = Number(rent) + Number(electric) + Number(water);
+console.log("The total cost of your apartment with your rent being $" + rent + " this month, your electric being $" + electric + " this month, and your water being $" + water + " this month is $" + totalApartment);
+
+//Caculate how much it cost per person
+var perPerson = totalApartment / 3;
+console.log(perPerson);
+
+//Testing my calculator
+//I tried
+//This is all wrong
 
 
 

2be96a75919e2e2909b007385e58b96b71d51984
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-15 19:44:00 -0500

    Learned about else if statements
    Testing multiple conditions together

--- Day 5/js/day5Conditional.js
+++ Day 5/js/day5Conditional.js
@@ -15,10 +15,12 @@ var oldEnough = true;
 //If the child is old enough then print to the console, You Can Ride!
 
 /*
-Basic Structure of if statement
+Basic Structure of if statement, else statement
 
 if(condition to test){
 Code to run if the condition is true
+} else{
+ Code to run if the condition is false
 }
 
 */
@@ -34,15 +36,64 @@ console.log("What comes after the if statement");
 //Relational Expression
 
 //If the kid is over 48" tall, then he can ride
-var kidHeight = 30;
+var kidHeight = 47;
 
 //Create variable for minimum height
 var minHeight = 48;
 
-if(kidHeight > minHeight){
+//Create a variable for shoe lift
+var shoeLift = 2;
+// Is the child tall enough with shoe lift?
+
+if(kidHeight + shoeLift > minHeight){
+
  console.log("You are tall enough to ride!");
+
 } else{
  //This code will run if the test is false!
  console.log("Sorry you are too short!");
+
 }
 
+
+
+//What ride can the kid go on based on his height?
+//If the kid 48 + then Space Mountain
+//If 40+ then Buzz Lightyear Ride
+//Anything short - Winnie The Pooh
+
+var childheight = 40;
+
+if(childheight >= 48){
+
+ console.log("You can ride Space Mountain, Buzz Lightyear, and Winnie The Pooh!");
+
+} else if (childheight >= 40){
+
+ console.log("You can ride Buzz Lightyear, and Winnie The Pooh!");
+
+} else {
+
+ console.log("You can only ride Winnie The Pooh!");
+
+}
+
+
+/*
+If, else if, else statement
+
+if(condition1 to test){
+
+ Code to run if condition1 is true.
+
+} else if(condition2 to test){
+
+ Condition2 will only be testing if condition1 is false!
+ Code to run if condition2 is true
+} else {
+
+ Code to run if condition1 and condition2 are false
+
+}
+
+*/

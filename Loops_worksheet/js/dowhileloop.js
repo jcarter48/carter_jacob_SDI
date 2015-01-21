3edf8dbfca9b4adac310c6bf57a39d15ab214f1b
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-20 21:51:11 -0500

    Starting Loops Worksheet
    Starting Loops Worksheet

---
+++ Loops_worksheet/js/dowhileloop.js
@@ -0,0 +1,8 @@
+/*
+ *Jacob Carter
+ *1/20/2015
+ *SDI Campus
+ *Do While Loop
+ */
+
+alert("Testing Scripts");
---
+++ Loops_worksheet/js/forloop.js
@@ -0,0 +1,8 @@
+/*
+ *Jacob Carter
+ *1/20/2015
+ *SDI Campus
+ *For Loop
+ */
+
+alert("Testing Scripts");

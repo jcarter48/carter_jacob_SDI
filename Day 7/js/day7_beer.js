131817e4ae76f824e4d8e379342a303132e66914
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-20 19:18:28 -0500

    Completed 99 Bottles of Beer
    Completed 99 Bottles of Beer

--- Day 7/js/day7_beer.js
+++ Day 7/js/day7_beer.js
@@ -7,3 +7,14 @@ Beer Counting
 
 //alert("Testing String");
 
+//99 Bottles of Beer on the wall
+
+for(var i = 99; i>0; i--){
+    
+    //Add a check for the last round of beers
+    if (i==1) {
+    console.log(i + " bottles of beer on the wall. " + i + " Bottles of beer. You take one down and pass it around, no more bottles of beer on the wall. \nThe End!") 
+    } else{
+    console.log(i + " bottles of beer on the wall. " + i + " Bottles of beer. You take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+    }
+}

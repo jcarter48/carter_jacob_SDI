4fa5dbef31df896011616393ee8adab07133314f
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-22 15:02:14 -0500

    Loops Worksheet
    Loops Worksheet, this obviously isn’t acceptable work for this
    assignment, I had an accident while updating my computer and lost my
    work(which shouldn’t madder because I swore I uploaded it to github
    wednesday). I can redo the activity if you give me time.

--- Loops_worksheet/js/dowhileloop.js
+++ Loops_worksheet/js/dowhileloop.js
@@ -5,4 +5,16 @@
  *Do While Loop
  */
 
-alert("Testing Scripts");
+//alert("Testing Scripts");
+
+var i=18;
+
+do{
+    //This code will run before the condition is checked
+    //It will also run as long as the condition is true
+    console.log("The number is " + i);
+    
+    i++;
+    
+    
+} while( i<=18 );
--- Loops_worksheet/js/whileloop.js
+++ Loops_worksheet/js/whileloop.js
@@ -5,4 +5,24 @@
  *While Loop
  */
 
-alert("Testing Scripts");
+//alert("Testing Scripts");
+
+//Lets ask the user their age
+var laps = prompt("Please enter how many laps a racecar can do before stopping:");
+
+//Validate that the user typed in a number
+while(isNaN(laps) || laps===""){
+    //This code runs anytime that the age is not a number
+    //Reprompt the user for a number
+    laps = prompt("Only use numbers, please enter how many laps a racercar do do before stopping:");
+}
+
+var counter = laps; //counter
+
+while (counter<=50) {
+    //Code that will run as long as counter <50
+    console.log("Racecars can drive for " + counter + " laps before making a pit stop");
+    
+    //Change for the counter variable
+    counter++;
+}

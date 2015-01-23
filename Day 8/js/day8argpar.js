1f8aa5276518f0db7bb2b90d9411b1bb1b05690a
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-22 19:39:30 -0500

    Created Arguments and Parameter files
    Created Arguments and Parameter files

---
+++ Day 8/js/day8argpar.js
@@ -0,0 +1,9 @@
+/*
+ *Jacob Carter
+ *1/22/2015
+ *SDI Campus
+ *Arguments and Parameters
+ */
+
+//alert("Testing!");
+
--- Day 8/js/day8scope.js
+++
@@ -1,8 +0,0 @@
-/*
- *Jacob Carter
- *1/22/2015
- *SDI Campus
- *Scope
- */
-
-alert("Testing");

f98a99cdd5c4258fddbb501eda1bab61a998aa71
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-01-22 22:20:39 -0500

    Finished Circumference
    completed the first problem

--- Functions_worksheet/js/functions_worksheet.js
+++ Functions_worksheet/js/functions_worksheet.js
@@ -7,3 +7,41 @@
 
 //alert("Testing");
 
+//calculating the circumference of a circle
+//Create a function to calculate the circumference of a circle
+//circumference = 2*pi*r
+function circleCircum(radius){
+    var circum = 2*Math.PI *radius;
+    
+    //return this area
+    return circum;
+    
+}
+
+//prompt user for the radius(the given)
+var userRad = prompt("Please type in a radius:");
+
+//validating if its a number/if its an empty string
+while(isNaN(userRad) || userRad===""){
+    userRad = prompt("Please only use numbers \nType in a radius:");
+}
+
+//parsing the variable
+userRad = parseInt(userRad);
+
+//variable to catch the results
+var circumference = circleCircum(userRad);
+
+//return:
+console.log(circumference);
+
+//result to print to the console
+console.log("The circumference of your circle with a radius of " + userRad + " is " + circumference.toFixed(2));
+
+//stung!
+//calculating how many bee sings are needed to kill an animal in a function
+
+
+
+
+
